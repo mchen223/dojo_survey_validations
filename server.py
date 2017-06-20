639dd3bc84462d0cc0e7539dfef9ff99e2dd5c13
Melissa Chen <chenmelissal@gmail.com>
2017-06-19 21:02:58 -0700

    Added validations

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 app = Flask(__name__)
 app.secret_key = 'SecretClubhousePassword'
 
@@ -8,9 +8,18 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-    return render_template('result.html', name=request.form['name'], location=request.form['location'],
-    language=request.form['language'],
-    comment=request.form['textarea'])
+    if len(request.form['name']) < 1:
+        flash("Error: Name needs to contain at least one letter.")
+        return render_template('index.html')
+    elif len(request.form['textarea']) > 120:
+        flash("Error: Comments are to be 120 characters max.")
+        return render_template('index.html')
+    else:
+        return render_template('result.html',
+        name=request.form['name'],
+        location=request.form['location'],
+        language=request.form['language'],
+        comment=request.form['textarea'])
     return redirect('/')
 
 @app.route('/back')
